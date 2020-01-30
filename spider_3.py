810e9c7bde20784b2ffc71042704f9d4f9c7d257
lynx <2104934038@qq.com>
2020-01-30 22:17:05 +0800

    todo:获取时间

--- spider_3.py
+++ spider_3.py
@@ -11,6 +11,7 @@ class Spider_season:
         url = 'https://api.bilibili.com/pgc/web/season/section?season_id={}'
         result = requests.get(url.format(season_id))
         return json.loads(result.text)
+
     def get_view(self, aid):
         # 获取番剧名
         url = 'https://api.bilibili.com/x/web-interface/view?aid={}'
@@ -22,11 +23,11 @@ class Spider_season:
             title = "仅限港澳台"
         else:
             title = view['data']['title']
-        return {"aid": aid,
-                "title": title}
+        return {"aid": aid, "title": title}
+
     def get_detail(self, season_id):
         # 获取详细信息
-        result = self.send_request() # 爆搜season
+        result = self.send_request(season_id) # 爆搜season
         item = {
                 "season_id": season_id,
                 "aid": 0,
@@ -38,17 +39,36 @@ class Spider_season:
                 "c_month": 0,
                 "c_day": 0
                 }
-        item.update(result)
         if result['code'] == 0: # 获取成功
             # if 'main_section' not in result['result']:
                 # TODO 异常情况
             aid = result['result']['main_section']['episodes'][0]['aid'] # TODO 获取aid
             view = self.get_view(aid) # 刷新aid, title
             item.update(view)
-        print(item)
+            print(item)
         return item
 
+    def insert_sql(self, cursor, item):
+        cmd = 'insert into season (season_id, aid, title, p_year, p_month, p_day, c_year, c_month, c_day) values' + \
+                '({}, {}, \'{}\', {}, {}, {}, {}, {}, {});'
+        cmd = cmd.format(
+            item['season_id'], 
+            item['aid'],
+            item['title'],
+            item['p_year'],
+            item['p_month'],
+            item['p_day'],
+            item['c_year'],
+            item['c_month'],
+            item['c_day']
+            )
+        cursor.execute(cmd)
+
 if __name__ == '__main__':
+    my_spider = Spider_season()
+    for i in range(1, 10):
+        my_spider.get_detail(i)
+
     db = pymysql.connect(
             host = 'localhost',
             port = 3306, # 端口错误错误代码111
@@ -58,19 +78,5 @@ if __name__ == '__main__':
             charset = 'utf8'
             )
     cursor = db.cursor()
-    cmd = 'insert into season (season_id, aid, title, p_year, p_month, p_day, c_year, c_month, c_day) values' + \
-            '({}, {}, \'{}\', {}, {}, {}, {}, {}, {});'
-    cmd = cmd.format(
-            test['season_id'], 
-            test['aid'],
-            test['title'],
-            test['p_year'],
-            test['p_month'],
-            test['p_day'],
-            test['c_year'],
-            test['c_month'],
-            test['c_day']
-            )
-    cursor.execute(cmd)
     db.commit()
     db.close()
