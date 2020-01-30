352dde6ab20454c32ec6a355aa5249390c1eea09
lynx <2104934038@qq.com>
2020-01-30 22:10:58 +0800

    todo:插入sql数据

--- spider_3.py
+++ spider_3.py
@@ -6,40 +6,47 @@ import requests
 import time
 
 class Spider_season:
-    def send_requests(self):
-        # 暴力发送request
+    def send_request(self, season_id):
+        # 获取aid
         url = 'https://api.bilibili.com/pgc/web/season/section?season_id={}'
-        for i in range(0, 5000):
-            result = requests.get(url.format(i))
-            yield [i, json.loads(result.text)]
+        result = requests.get(url.format(season_id))
+        return json.loads(result.text)
     def get_view(self, aid):
         # 获取番剧名
         url = 'https://api.bilibili.com/x/web-interface/view?aid={}'
         result = requests.get(url.format(aid))
-        res_json = json.loads(result.text)
+        view = json.loads(result.text)
         title = "NULL"
-        if 'data' not in res_json:
+        if 'data' not in view:
             # 异常情况
             title = "仅限港澳台"
         else:
-            title = res_json['data']['title']
+            title = view['data']['title']
         return {"aid": aid,
                 "title": title}
-    def get_results(self):
-        # 筛选爆搜结果
-        for result in self.send_requests():
-            season_id = result[0]
-            response = result[1] # json
-            if response['code'] == 0: # 获取成功
-                if 'main_section' not in response['result']:
-                    # 异常情况
-                    view = {"season_id": season_id}
-                else:
-                    aid = response['result']['main_section']['episodes'][0]['aid']
-                    # TODO 获取aid
-                    view = self.get_view(aid)
-                    view.update({"season_id": season_id})
-                print(view)
+    def get_detail(self, season_id):
+        # 获取详细信息
+        result = self.send_request() # 爆搜season
+        item = {
+                "season_id": season_id,
+                "aid": 0,
+                "title": "没有标题",
+                "p_year": 0,
+                "p_month": 0,
+                "p_day": 0,
+                "c_year": 0,
+                "c_month": 0,
+                "c_day": 0
+                }
+        item.update(result)
+        if result['code'] == 0: # 获取成功
+            # if 'main_section' not in result['result']:
+                # TODO 异常情况
+            aid = result['result']['main_section']['episodes'][0]['aid'] # TODO 获取aid
+            view = self.get_view(aid) # 刷新aid, title
+            item.update(view)
+        print(item)
+        return item
 
 if __name__ == '__main__':
     db = pymysql.connect(
@@ -51,17 +58,6 @@ if __name__ == '__main__':
             charset = 'utf8'
             )
     cursor = db.cursor()
-    test = {
-            "season_id": 0,
-            "aid": 0,
-            "title": "爱上对方就卡死了地方就是打卡了",
-            "p_year": 0,
-            "p_month": 0,
-            "p_day": 0,
-            "c_year": 0,
-            "c_month": 0,
-            "c_day": 0
-            }
     cmd = 'insert into season (season_id, aid, title, p_year, p_month, p_day, c_year, c_month, c_day) values' + \
             '({}, {}, \'{}\', {}, {}, {}, {}, {}, {});'
     cmd = cmd.format(
